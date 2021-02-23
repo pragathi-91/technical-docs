2d771e61552bec89197ef54113bf3e6052685d71
jangam pragathi <79441233+pragathi-91@users.noreply.github.com>
2021-02-23 16:44:25 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -17,9 +17,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'ABC'
-copyright = '2021, Harideep'
-author = 'Harideep'
+project = 'git & github documentation'
+copyright = '2021, pragathi'
+author = 'pragathi'
 
 # The full version, including alpha/beta/rc tags
 release = '2021'
